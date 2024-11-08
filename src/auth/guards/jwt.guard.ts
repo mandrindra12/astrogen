1dc9e04499d1d94465904d425b110f17620a34f2
mandrindra12 <mandrindraantonnio@gmail.com>
2024-11-08 15:54:26 +0300

    restrict request

--- src/auth/guards/jwt.guard.ts
+++ src/auth/guards/jwt.guard.ts
@@ -3,10 +3,11 @@ import {
   Injectable
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtGuard extends AuthGuard('jwt') {
-  async canActivate(context: ExecutionContext) {
-    return true;
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    return super.canActivate(context);
   }
 }
